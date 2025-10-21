4f8f68d0a7b3612c187caaff4b057159b5d72ad5
gustii17 <gustavooliveiraseabra@gmail.com>
2025-10-21 13:47:33 -0300

    test

--- software/compilador/escrita.cpp
+++ software/compilador/escrita.cpp
@@ -13,14 +13,15 @@ int main(){
     string line = "000";
     unsigned char byt = 0x40;
 
-    arq.open ("example.bin", ios::out | ios::trunc | ios::binary);
+    arq.open ("exampl.bin", ios::out | ios::trunc | ios::binary);
 
 
     if(!arq.is_open()){
         cout << "arquivo nao aberto";
         return 404;
     }
-    arq.write((char*) (&byt), 1);
+    //arq.write((char*) (&byt), 1);
+    arq.write((char*) line.c_str(), 8);
     
     arq.close();
     
